0b12c15c4a5a5ab9047ff4118512ef8c4bf0c91d
sarika002 <98938893+sarika002@users.noreply.github.com>
2022-02-07 16:40:49 +0530

    Add files via upload

--- Interface Segragation Principle/shape.py
+++ Interface Segragation Principle/shape.py
@@ -1,1 +1,23 @@
+class shape:
+    def __init__(self,shape):
+        self._shape_name=shape
 
+    def get_shape_name(self):
+        return self._shape_name
+
+class circle(shape):
+    def cal_area(self,radius):
+        return (3.14)*radius**2
+
+
+class square(shape):
+    def cal_area(self,side):
+        return side*side
+
+cir=circle("circle")
+print("The shape is: ",cir.get_shape_name())
+print("Area of circle: ",cir.cal_area(3.5))
+
+sq=square("square")
+print("The shape is: ",sq.get_shape_name())
+print("Area of square: ",sq.cal_area(4))
