bd12edfda8d8b00cc097f9a33e2a18bf27c715f8
sarika002 <98938893+sarika002@users.noreply.github.com>
2022-02-07 18:13:20 +0530

    Add files via upload

--- Dependency Inversion Principle/employee.py
+++ Dependency Inversion Principle/employee.py
@@ -1,1 +1,35 @@
+class employee:   # A high-level class
+    def __init__(self):
+        self.developers=Developer()
+
+    def develop(self):
+        return self.developers.develops()
+
+class Developer:   # Abstract class
+    def __init__(self):
+        self.front=frontend()
+        self.back=backend()
+
+    def develops(self):
+        self.front.develop()
+        self.back.develop()
+
+class frontend:    # A low-level class
+    def develop(self):
+        self.display()
+
+    def display(self):
+        print("I am a Frontend Developer")
+
+
+class backend:    # a low-level class
+    def develop(self):
+        self.display()
+
+    def display(self):
+        print("I am a Backend developer")
+
+
+developer=employee()
+developer.develop()
 
